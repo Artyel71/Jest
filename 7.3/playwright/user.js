8ca50ce236339fe0ecf5d07fccf0054193a8b1f6
Artyel <artyel@icloud.com>
2023-09-15 14:24:31 +0100

    ignore added

--- 7.3/playwright/user.js
+++ 7.3/playwright/user.js
@@ -1,1 +1,10 @@
+const email = "lenovos6000v4@gmail.com"
+const password = "AlexBaskey/246"
+exports.email = email;
+exports.password = password;
+const wrongEmail = "fdryu875@gmail.com"
+const wrongPassword = "FGHJK876"
+exports.wrongEmail = wrongEmail;
+exports.wrongPassword = wrongPassword;
 
+trt
