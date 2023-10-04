47709426783bc53719146a31bea10759d5ed99e7
Artyel71 <124166581+Artyel71@users.noreply.github.com>
2023-10-04 08:54:12 +0100

    Add files via upload

--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -1,8 +1,11 @@
 const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
-const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
+const { putText, getText, days, movieTime, seats,clickElement,
+ } = require("../../lib/commands.js");
+
+setDefaultTimeout(60000);
 
 Before(async function () {
   const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
@@ -18,17 +21,52 @@ After(async function () {
 });
 
 Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php${string}`, {
     setTimeout: 20000,
   });
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+When("user choose day {string}", async function (string) {
+  return await days(this.page, string);
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
+When(
+  "user choose movie {string} and movieTime {string}",
+  async function (string, string2) {
+    return await movieTime(this.page, string, string2);
+  }
+);
+
+When(
+  "user choose seats row {string} and seat {string}",
+  async function (string, string2) {
+    return await seats(this.page, string, string2);
+  }
+);
+
+When("user click book {string}", async function (string) {
+  return await clickElement(this.page, string);
+});
+
+Then("user sees booking confirmation {string}", async function (string) {
+  const actual = await getText(this.page, ".ticket__check-title");
   const expected = await string;
   expect(actual).contains(expected);
 });
+
+Then("user sees the header {string}", async function (string) {
+  const actual = await getText(this.page, "h2");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Then(
+  "user sees {string} is not clickable",
+  async function (string) {
+    const acceptionButton = await this.page.$eval(
+      "button",
+      (button) => button.disabled
+    );
+    await expect(acceptionButton).to.be.true;
+  }
+);
